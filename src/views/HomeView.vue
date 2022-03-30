8525373fc41b3819ee6ab8d50a34afa335a5f88a
sastra12 <anonymousonly594@gmail.com>
2022-03-30 11:04:59 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderBeetwen />
+    <HeroSection />
+    <BannerSection />
+    <InstaSection />
+    <PartnerSection />
+    <FooterSection />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HeaderBeetwen from "@/components/HeaderBeetwen.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import BannerSection from "@/components/BannerSection.vue";
+import InstaSection from "@/components/Insta.vue";
+import PartnerSection from "@/components/Partner.vue";
+import FooterSection from "@/components/FooterSection.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderBeetwen,
+    HeroSection,
+    BannerSection,
+    InstaSection,
+    PartnerSection,
+    FooterSection,
+  },
+};
+</script>
